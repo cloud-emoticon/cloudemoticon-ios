810542b3682326fd5b39f02d6751390d3bb56f21
神楽坂雅詩 <cxchope@gmail.com>
2014-07-08 11:57:56 +0800

    更新證書

---
+++ CloudEmoticon2iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CloudEmoticon2iOS
+//
+//  Created by 神楽坂雅詩 on 14/7/8.
+//  Copyright (c) 2014年 神楽坂雅詩 & 神楽坂紫喵. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
