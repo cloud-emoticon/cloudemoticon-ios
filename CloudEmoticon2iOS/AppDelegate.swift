b56be6501918c6f2b7ba2458257bd223711efa27
神楽坂雅詩 <cxchope@gmail.com>
2014-08-05 17:44:24 +0800

    成功下载数据

--- CloudEmoticon2iOS/AppDelegate.swift
+++ CloudEmoticon2iOS/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
         // Override point for customization after application launch.
+        
+        //TEST
+        var newdwn = NetworkDownload.alloc()
+        newdwn.startAsyConnection()
+        //TEST
+        
         return true
     }
 
--- CloudEmoticon2iOS/NetworkDownload.swift
+++ CloudEmoticon2iOS/NetworkDownload.swift
@@ -7,7 +7,92 @@
 //
 
 import UIKit
+import Foundation
+class NetworkDownload: NSObject, NSURLConnectionDelegate, NSObjectProtocol {
+    
+    var connData: NSMutableData
+    
+    init(connData: NSMutableData)
+    {
+        self.connData = NSMutableData.data();
+    }
+    
+    
+    //开始连接
+//    func startConnection()
+//    {
+//    }
+    
+    //开始异步连接
+    func startAsyConnection()
+    {
+        var urla: NSURL
+        urla = NSURL.URLWithString("http://www.heartunlock.com/ce.xml")
+        var request: NSURLRequest
+        request = NSURLRequest(URL:urla)
+//        request.URL = urla
+//        request.HTTPMethod = "POST"
+        NSURLConnection.connectionWithRequest(request, delegate: self)
+    }
+    
+//    func connection(connection: NSURLConnection!, willSendRequest request: NSURLRequest!, redirectResponse response: NSURLResponse!) -> NSURLRequest!
+//    {
+//        print("1")
+//        return request
+//    }
+    
+    //成功序列：服务器响应、接收数据、缓存、成功接收
+    //失败序列：
+    
+    //服务器响应
+    func connection(connection: NSURLConnection!, didReceiveResponse response: NSURLResponse!)
+    {
+        print("2")
+    }
+    //接收数据
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!)
+    {
+        print("3")
+        if (self.connData == nil)
+        {
+            self.connData = NSMutableData.data();
+        }
+        self.connData.appendData(data)
+        //println(data)
+    }
+//    func connection(connection: NSURLConnection!, needNewBodyStream request: NSURLRequest!) -> NSInputStream!
+//    {
+//        print("4")
+//        return NSInputStream.alloc();
+//    }
+//    func connection(connection: NSURLConnection!, didSendBodyData bytesWritten: Int, totalBytesWritten: Int, totalBytesExpectedToWrite: Int)
+//    {
+//        print("5")
+//    }
+//    //缓存
+//    func connection(connection: NSURLConnection!, willCacheResponse cachedResponse: NSCachedURLResponse!) -> NSCachedURLResponse!
+//    {
+//        print("6")
+//        return NSCachedURLResponse.alloc()
+//    }
 
-class NetworkDownload: NSObject {
-   
+    //成功接收
+    func connectionDidFinishLoading(connection: NSURLConnection!)
+    {
+        print("7")
+        if (self.connData != nil)
+        {
+            var str:NSString
+            str = NSString(data: self.connData, encoding: NSUTF8StringEncoding)
+            println(str)
+        } else {
+            println("NULL!!!")
+        }
+    }
+    
+    //接受失败
+    func connection(connection: NSURLConnection!, didFailWithError error: NSError!)
+    {
+        print("ERR")
+    }
 }
