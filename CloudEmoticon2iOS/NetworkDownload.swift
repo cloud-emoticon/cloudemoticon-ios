7e88f415a3b2e244d3439008c2ff321ed55de09d
神楽坂雅詩 <cxchope@gmail.com>
2014-08-05 15:42:21 +0800

    修复未知问题

---
+++ CloudEmoticon2iOS/NetworkDownload.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkDownload.swift
+//  CloudEmoticon2iOS
+//
+//  Created by 神楽坂雅詩 on 14/8/5.
+//  Copyright (c) 2014年 神楽坂雅詩 & 神楽坂紫喵. All rights reserved.
+//
+
+import UIKit
+
+class NetworkDownload: NSObject {
+   
+}
