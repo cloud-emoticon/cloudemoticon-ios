afdabf3d7e0b6d45a073136559f1dab14a3e2098
神楽坂紫 <nashimotomasayuki@gmail.com>
2014-08-26 23:22:44 +0800

    一口气搞定所有要出现的窗口的push

---
+++ SetTableViewController.swift
@@ -0,0 +1,133 @@
+//
+//  SetTableViewController.swift
+//  CloudEmoticon2iOS
+//
+//  Created by 神楽坂紫 on 14/8/26.
+//  Copyright (c) 2014年 神楽坂雅詩 & 神楽坂紫喵. All rights reserved.
+//
+
+import UIKit
+
+class SetTableViewController: UITableViewController {
+
+    
+    var list:NSMutableArray = NSMutableArray.array()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        list.addObject("个性化")
+        list.addObject("源管理")
+        list.addObject("设置")
+        list.addObject("关于")
+
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return 4
+    }
+
+    
+    override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("SetNav", forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel.text = list.objectAtIndex(indexPath.row) as NSString
+
+        return cell
+    }
+
+    override func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        let storyboard = self.storyboard
+        
+        switch indexPath.row {
+        case 0:
+            let push:UIViewController = storyboard.instantiateViewControllerWithIdentifier("Color") as UIViewController
+            self.navigationController.pushViewController(push, animated: true)
+            break
+        case 1:
+            self.navigationController.pushViewController(ScoreTableViewController(), animated: true)
+            break
+        case 2:
+            let push:UIViewController = storyboard.instantiateViewControllerWithIdentifier("Setting") as UIViewController
+            self.navigationController.pushViewController(push, animated: true)
+            break
+        case 3:
+            let push:UIViewController = storyboard.instantiateViewControllerWithIdentifier("About") as UIViewController
+            self.navigationController.pushViewController(push, animated: true)
+            break
+        default:
+            break
+        
+        }
+    
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView!, canEditRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView!, moveRowAtIndexPath fromIndexPath: NSIndexPath!, toIndexPath: NSIndexPath!) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView!, canMoveRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
