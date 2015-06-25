e4c1f1f46606b84535cc11a7cb18f436e14b749c
神楽坂雅詩 <cyscxchope@gmail.com>
2015-06-25 15:33:12 +0800

    登录和注册页面可以根据软键盘的弹出而移动位置防止遮挡
    iPad未产生遮挡不会受影响。回车按钮根据Next和Done决定下一个输入框和关闭软键盘。已对横屏进行兼容。

--- CloudEmoticon2iOS/LoginViewController.swift
+++ CloudEmoticon2iOS/LoginViewController.swift
@@ -8,19 +8,59 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var 账号输入框: UITextField!
+    @IBOutlet weak var 密码输入框: UITextField!
+    @IBOutlet weak var 注册按钮: UIButton!
+    @IBOutlet weak var 登录按钮: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "登陆"  //lang.uage("登陆")
-        // Do any additional setup after loading the view.
+        let 背景色:UIColor = UIColor(red: 1, green: 0.79215, blue: 0.86274, alpha: 1)
+        self.navigationController?.view.backgroundColor = 背景色
+        self.view.backgroundColor = 背景色
+        账号输入框.delegate = self
+        账号输入框.keyboardType = UIKeyboardType.NamePhonePad
+        密码输入框.delegate = self
+    }
+    
+    @IBAction func 登录按钮点击(sender: UIButton) {
+        
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if (self.view.frame.origin.y == 0 && self.view.frame.size.height < 1024) {
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                self.view.frame.origin.y = 0 - textField.frame.origin.y + self.navigationController!.navigationBar.frame.size.height + 25
+                //self.view.frame.origin.y = 0 - self.view.frame.size.height * 0.25
+            })
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if (账号输入框.isFirstResponder()) {
+            密码输入框.becomeFirstResponder()
+        } else if (密码输入框.isFirstResponder()) {
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        if (self.view.frame.origin.y < 0 && self.view.frame.size.height < 1024) {
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                self.view.frame.origin.y = 0
+            })
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
--- RegViewController.swift
+++ RegViewController.swift
@@ -8,19 +8,31 @@
 
 import UIKit
 
-class RegViewController: UIViewController {
+class RegViewController: UIViewController, UITextFieldDelegate {
 
-    @IBOutlet weak var mail: UITextField!
-    @IBOutlet weak var username: UITextField!
-    @IBOutlet weak var password: UITextField!
-    @IBOutlet weak var passconfirm: UITextField!
+    @IBOutlet weak var 邮箱输入框: UITextField!
+    @IBOutlet weak var 用户名输入框: UITextField!
+    @IBOutlet weak var 密码输入框: UITextField!
+    @IBOutlet weak var 确认密码输入框: UITextField!
     @IBOutlet weak var reset: UIButton!
     @IBOutlet weak var reg: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let 背景色:UIColor = UIColor(red: 1, green: 0.79215, blue: 0.86274, alpha: 1)
+        self.navigationController?.view.backgroundColor = 背景色
+        self.view.backgroundColor = 背景色
+        邮箱输入框.delegate = self
+        邮箱输入框.keyboardType = UIKeyboardType.EmailAddress
+        用户名输入框.delegate = self
+        用户名输入框.keyboardType = UIKeyboardType.NamePhonePad
+        密码输入框.delegate = self
+        确认密码输入框.delegate = self
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func 注册按钮点击(sender: UIButton) {
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,22 +41,40 @@ class RegViewController: UIViewController {
     }
     
     @IBAction func 重置按钮(sender: AnyObject) {
-        mail.text = ""
-        username.text = ""
-        password.text = ""
-        passconfirm.text = ""
-        mail.becomeFirstResponder()
+        邮箱输入框.text = ""
+        用户名输入框.text = ""
+        密码输入框.text = ""
+        确认密码输入框.text = ""
+//        邮箱输入框.becomeFirstResponder()
+    }
+
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if (self.view.frame.origin.y == 0 && self.view.frame.size.height < 1024) {
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                self.view.frame.origin.y = 0 - textField.frame.origin.y + self.navigationController!.navigationBar.frame.size.height + 25
+                //self.view.frame.origin.y = 0 - self.view.frame.size.height * 0.25
+            })
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if (邮箱输入框.isFirstResponder()) {
+            用户名输入框.becomeFirstResponder()
+        } else if (用户名输入框.isFirstResponder()) {
+            密码输入框.becomeFirstResponder()
+        } else if (密码输入框.isFirstResponder()) {
+            确认密码输入框.becomeFirstResponder()
+        } else if (确认密码输入框.isFirstResponder()) {
+            textField.resignFirstResponder()
+        }
+        return true
     }
-    */
-
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        if (self.view.frame.origin.y < 0 && self.view.frame.size.height < 1024) {
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                self.view.frame.origin.y = 0
+            })
+        }
+    }
 }
